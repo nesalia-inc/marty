33c4a3a88ad7c0658ff549abb3da024907e99af4
martyy-code <nesalia.inc@gmail.com>
2026-08-12 14:20:01 +0200

    Update

--- agent/channels/github.ts
+++ agent/channels/github.ts
@@ -1,7 +1,7 @@
 import { githubChannel } from "eve/channels/github";
 
 export default githubChannel({
-  botName: "martyy-code",
+  botName: "marty-action",
   credentials: {
     appId: process.env.GITHUB_APP_ID,
     privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
